3b53d2c4c0762008dd852695482d23fa21a961b6
Daniel Rhodes <danielrhodesnz@gmail.com>
2020-11-04 13:02:31 +1300

    botJerry is online

---
+++ main.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js')
+
+const client = new Discord.Client()
+
+
+client.once('ready', () => {
+    console.log('botJerry is ready!');
+})
+
+//This must be the last line
+client.login('NzczMzI5ODY0OTMyOTgyODE2.X6Hpjw.mwtNuQCoscikQVVEWXMEdkDWFwE')
+
